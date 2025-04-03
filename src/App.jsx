39f077f4b30f92ae066c4f08deffeea64be980ae
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-03 14:45:35 +0500

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import { useReducer } from "react";
 
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+];
 
+const reducer = (state, action) => {
+  switch(action.type){
+    case "DONE":
+      return state.map((item)=> {
+        if(item.id === action.id){
+          return {...item, complete: !item.complete}
+        }else{
+          return item
+        }
+      })
+      default:
+    return state
+  }
+}
 const App = () => {
+ const [todos, dispatch] = useReducer(reducer, initialTodos)
+
+ const handleComplete = (todo)=> {
+    dispatch({type:"DONE", id:todo.id})
+ }
   return (
-    <div>App</div>
+    <>
+      {todos.map((item)=> (
+        <div key={item.id}>
+          <label>
+             <input type="checkbox" checked={item.complete} onChange={()=> handleComplete(item)}/>
+             {item.title}
+          </label>
+        </div>
+      ))}
+    </>
   )
 }
 
